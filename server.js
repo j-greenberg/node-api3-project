0b03226ca0bd5d96e3088e812c5d986ec8f26f8e
j-greenberg <jgreenberg@runbox.com>
2020-04-08 16:44:56 -0400

    updates

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-const express = require('express');
+const express = require('express'); 
 
 const server = express();
+
+const userRouter = require("./users/userRouter.js"); 
+const postRouter = require("./posts/postRouter.js"); 
+
+const userDb = require("./users/userDb"); 
+
+server.use(logger); 
+server.use("/users", userRouter); 
+server.use("/posts", postRouter); 
+
+server.use(express.json());
+
+//custom middleware
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+function logger(req, res, next) {
+  
+  userDb.get()
+  .then(response => {
+    console.log(response); 
+  })
+  .catch(error => {
+    console.log(error); 
+  }); 
 
-function logger(req, res, next) {}
+  console.log(`Method: ${req.method}`); 
+  console.log(`Original URL: ${req.originalUrl}`); 
+  console.log(`URL: ${req.url}`);
+  console.log(`Timestamp: ${new Date().toTimeString()}`);
+  next(); 
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const userDb = require("./userDb.js"); 
+
 const router = express.Router();
+
+router.use(validateUserId); 
 
 router.post('/', (req, res) => {
   // do your magic!
@@ -34,10 +38,28 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  userDb
+  .getById(req.params.id)
+  .then(response => {
+    if(response){
+      console.log("user is found"); 
+      console.log(response); 
+      req.user = response;          
+      next(); 
+    } else {
+      console.log("User is not found"); 
+    }
+  })
+  .catch(error => {
+    console.log(error); 
+    res.status(400).json({ message: "invalid user id" }); 
+  })
 }
+
 
 function validateUser(req, res, next) {
   // do your magic!
+
 }
 
 function validatePost(req, res, next) {
